303de6d37c91a75e340b985ebe5a29366949ac9b
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-06-04 20:19:03 +0900

    fix configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: %i[top about]
+  before_action :authenticate_user!, except: %i[top about] # rubocop:disable  Rails/LexicallyScopedActionFilter
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  def get_profile_image
+  def get_profile_image # rubocop:disable Naming/AccessorMethodName
     profile_image.attached? ? profile_image : 'no_image.jpg'
   end
 end
