6de8f42c95aa29148cb51b5666f92ddb8ba7ddad
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-06-04 22:32:49 +0900

    add rspec for favorite validation

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,5 +4,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :book_id, uniqueness: { scope: :user_id }
+  validates :book_id, uniqueness: { scope: :user_id } # rubocop:disable Rails/UniqueValidationWithoutIndex
 end
