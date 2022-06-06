96b0f7bd6c4070bb13a5b2853a874d947baea5e8
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-06-06 21:46:21 +0900

    add relationship model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, class_name: 'User', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :user
+  has_many :relationships, class_name: 'User', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :user
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
