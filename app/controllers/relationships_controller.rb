84e42b87d0e1c60f89d4b59387e6b8fe04683658
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-06-06 23:20:40 +0900

    add create and destroy relationship on show

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class RelationshipsController < ApplicationController
-  def create; end
+  def create
+    relationship = Relationship.new(follower_id: current_user.id, followed_id: params[:user_id])
+    relationship.save
+    redirect_to request.referer
+  end
 
-  def destroy; end
+  def destroy
+    relationship = Relationship.find_by(follower_id: current_user.id, followed_id: params[:user_id])
+    relationship.destroy
+    redirect_to request.referer
+  end
 end
