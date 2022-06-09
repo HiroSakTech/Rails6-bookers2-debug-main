181460ef05ce163ae546eeab207712107d73868c
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-06-09 23:42:23 +0900

    add async book comment functions

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,19 +2,18 @@
 
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
 
   def destroy
-    book_comment = BookComment.find(params[:id])
-    redirect_to book_path(book_comment.book_id) and return if book_comment.user_id != current_user.id
+    @book_comment = BookComment.find(params[:id])
+    @book = @book_comment.book
+    redirect_to book_path(@book_comment.book_id) and return if @book_comment.user_id != current_user.id
 
-    book_comment.destroy
-    redirect_to request.referer
+    @book_comment.destroy
   end
 
   private
