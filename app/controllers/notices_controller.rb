147994aa7c21d42f39d9fb697b8df1e31e0fa556
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-06-13 18:57:57 +0900

    add new page for notice

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class NoticesController < ApplicationController
   def new
+    @group = Group.find(params[:group_id])
+    @notice = Notice.new
   end
 
-  def create
-  end
+  def create; end
 end
