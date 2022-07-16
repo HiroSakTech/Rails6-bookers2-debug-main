daecdf1e521f38a3bec99b03fa3a1c29247d1120
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-07-16 13:49:56 +0900

    restrict for edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: %i[edit update]
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
     @user = User.find(params[:id])
@@ -23,6 +24,13 @@ class UsersController < ApplicationController
       redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
     else
       render 'edit'
+    end
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
     end
   end
 
