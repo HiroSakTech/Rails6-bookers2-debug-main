1329e4b26511b5f40ceaf979d6eec2571764ad3e
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-07-16 13:46:49 +0900

    add guest login

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   resources :books, only: %i[index show edit create destroy update] do
     resource :favorites, only: %i[create destroy]
     resources :book_comments, only: %i[create destroy]
